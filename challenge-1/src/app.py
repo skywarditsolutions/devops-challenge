79c043a229da0b5fd0133a1a578f404ec6d87f62
jwac <josh@sirwac.net>
2024-08-18 16:20:56 -0400

    challenge1 mostly complete optionals to come later

---
+++ challenge-1/src/app.py
@@ -0,0 +1,23 @@
+## Libraries
+import argparse
+
+from flask import Flask
+
+## Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--service-address', type=str, help="Address to listen on")
+parser.add_argument('--service-port', type=int, help="Port to listen on")
+args = parser.parse_args()
+
+# Initiate Flask app
+app = Flask(__name__)
+
+# Define routes
+@app.route('/')
+def root():
+    message = f"Hello, Skyward Solutions!\n"
+    return message    
+
+if __name__ == "__main__":
+    # Run web service
+    app.run(host=args.service_address, port=args.service_port)
